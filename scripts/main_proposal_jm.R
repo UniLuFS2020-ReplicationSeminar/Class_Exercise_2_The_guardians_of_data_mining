a6a305c6efd181a8b4540d565f55a3dd75a2e7bd
janmurer <jan.murer@bluewin.ch>
2024-04-30 16:12:30 +0200

    Script proposal for loading API credentials, but not pushing them

--- scripts/main_proposal_jm.R
+++ scripts/main_proposal_jm.R
@@ -1,7 +1,10 @@
 library(here)
 library(tidyverse)
+library(httr)
 
-# Save your API key in a .csv file in the same directory as this script
+# Save your API key in a api_credentials.csv file in the folder api_key. 
 
-# Load the API key
+# Load the API key and store in a variable
 
+api_key <- read_csv(here("api_key/api_credentials.csv")) %>% 
+  pull(api_key)
